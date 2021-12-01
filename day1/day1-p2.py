749ea35d7e008231f99c7f44a51f4455628f16e7
theyeeman <fernando_y767@hotmail.com>
2021-12-01 17:32:10 -0500

    brute force

--- day1/day1-p2.py
+++ day1/day1-p2.py
@@ -5,9 +5,12 @@ def getInput(filename):
 if __name__ == "__main__":
     depths = getInput('input.txt')
     num_increase = 0
+    prev_depth = depths[0] + depths[1] + depths[2]
 
-    for i in range(1, len(depths)):
-        if depths[i] > depths[i - 1]:
+    for i in range(3, len(depths)):
+        curr_depth = depths[i] + depths[i - 1] + depths[i - 2]
+        if curr_depth > prev_depth:
             num_increase += 1
+        prev_depth = curr_depth
 
     print(num_increase)
