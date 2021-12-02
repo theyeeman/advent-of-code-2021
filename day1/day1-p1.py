5f5d284e0e7b085d305c7b5c598639bffb63fc67
theyeeman <fernando_y767@hotmail.com>
2021-12-01 22:30:56 -0500

    refactor part 1

--- day1/day1-p1.py
+++ day1/day1-p1.py
@@ -1,9 +1,7 @@
-def getInput(filename):
-    with open(filename) as f:
-        return list(map(lambda a : int(a), list((f.read()).split("\n"))))
+import time
 
-if __name__ == "__main__":
-    depths = getInput('input.txt')
+def main():
+    depths = get_input('input.txt')
     num_increase = 0
 
     for i in range(1, len(depths)):
@@ -11,3 +9,18 @@ if __name__ == "__main__":
             num_increase += 1
 
     print(num_increase)
+
+# Boilerplate code below
+
+def get_input(filename):
+    with open(filename) as f:
+        return list((f.read()).split("\n"))
+
+def print_performance(start, end):
+    print('Execution time (s):', round((end - start), 3))
+
+if __name__ == "__main__":
+    perf_counter_start = time.perf_counter()
+    main()
+    perf_counter_end = time.perf_counter()
+    print_performance(perf_counter_start, perf_counter_end)
