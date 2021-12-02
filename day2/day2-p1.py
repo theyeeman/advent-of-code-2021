8ab9521274cd306aeaef314adccaed360ffd3c41
theyeeman <fernando_y767@hotmail.com>
2021-12-02 08:14:55 -0500

    part 1 done

---
+++ day2/day2-p1.py
@@ -0,0 +1,41 @@
+import time
+
+def main():
+    # commands = get_input_as_str('test.txt')
+    commands = get_input_as_str('input.txt')
+    h_pos = 0
+    depth = 0
+    
+    for command in commands:
+        direction, distance = command.split(' ')[0], int(command.split(' ')[1])
+
+        match direction:
+            case 'forward':
+                h_pos += distance
+            case 'up':
+                depth -= distance
+            case 'down':
+                depth += distance
+            case _:
+                print('invalid')
+        
+    print(h_pos * depth)
+
+# Boilerplate code below
+
+def get_input_as_int(filename):
+    with open(filename) as f:
+        return list(map(lambda a : int(a), list((f.read()).split("\n"))))
+
+def get_input_as_str(filename):
+    with open(filename) as f:
+        return list((f.read()).split("\n"))
+
+def print_performance(start, end):
+    print('Execution time (s):', round((end - start), 3))
+
+if __name__ == "__main__":
+    perf_counter_start = time.perf_counter()
+    main()
+    perf_counter_end = time.perf_counter()
+    print_performance(perf_counter_start, perf_counter_end)
