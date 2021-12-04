74335a4bec915907e82517dfd959d27c44081c00
theyeeman <fernando_y767@hotmail.com>
2021-12-03 22:06:13 -0500

    day3 nit fix

--- day3/day3-p2.py
+++ day3/day3-p2.py
@@ -82,12 +82,15 @@ class standard_func:
         with open(filename) as f:
             return list(map(lambda a : int(a), list((f.read()).split("\n"))))
 
+
     def get_input_as_str(filename):
         with open(filename) as f:
             return list((f.read()).split("\n"))
 
+
     def print_performance(start, end):
         print('Execution time (s):', round((end - start), 3))
+
 
 if __name__ == "__main__":
     perf_counter_start = time.perf_counter()
