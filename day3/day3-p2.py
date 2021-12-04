453edf4916932957d2d5d68ba6680a61c79eb359
theyeeman <fernando_y767@hotmail.com>
2021-12-03 22:05:11 -0500

    fix day3 boilerplate code

--- day3/day3-p2.py
+++ day3/day3-p2.py
@@ -66,8 +66,8 @@ def remove_row(inputs, char_pos, bin_val):
 
 
 def main():
-    inputs = get_input_as_str('input.txt')
-    #inputs = get_input_as_str('test.txt')
+    inputs = standard_func.get_input_as_str('input.txt')
+    #inputs = standard_func.get_input_as_str('test.txt')
     backup = inputs.copy()
 
     oxygen_rating = int(get_oxygen_rating(backup), 2)
@@ -77,19 +77,20 @@ def main():
 
 
 # Boilerplate code below
-def get_input_as_int(filename):
-    with open(filename) as f:
-        return list(map(lambda a : int(a), list((f.read()).split("\n"))))
+class standard_func:
+    def get_input_as_int(filename):
+        with open(filename) as f:
+            return list(map(lambda a : int(a), list((f.read()).split("\n"))))
 
-def get_input_as_str(filename):
-    with open(filename) as f:
-        return list((f.read()).split("\n"))
+    def get_input_as_str(filename):
+        with open(filename) as f:
+            return list((f.read()).split("\n"))
 
-def print_performance(start, end):
-    print('Execution time (s):', round((end - start), 3))
+    def print_performance(start, end):
+        print('Execution time (s):', round((end - start), 3))
 
 if __name__ == "__main__":
     perf_counter_start = time.perf_counter()
     main()
     perf_counter_end = time.perf_counter()
-    print_performance(perf_counter_start, perf_counter_end)
+    standard_func.print_performance(perf_counter_start, perf_counter_end)
