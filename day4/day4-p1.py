840bd580029b364b30d2869b218572b7c5518ba1
theyeeman <fernando_y767@hotmail.com>
2021-12-04 12:51:51 -0500

    remove brainstorm text

--- day4/day4-p1.py
+++ day4/day4-p1.py
@@ -115,22 +115,3 @@ if __name__ == "__main__":
     main()
     perf_counter_end = time.perf_counter()
     standard_func.print_performance(perf_counter_start, perf_counter_end)
-
-'''
-Brainstorm
-
-need function to parse input into bingo numbers and list of bingo boards
-    bingo boards can be 2d arrays
-
-need function to check for line on a bingo board
-    2 checks
-        horizontal line, vertical line (diagonals don't count)
-    since i dont need to keep track of marked numbers, can probably just overwrite then with a '-'
-
-need function to mark bingo card
-    nested for loop to look at all numbers
-
-should i mark number then check for line for each bingo board? or mark all bingo boards then check all bingo boards?
-    there might be a way to make things more efficient, but for now just brute force
-
-'''
