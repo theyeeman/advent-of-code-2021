447e38cd028e4dcc871cefdc60805eb0a50b7ee1
theyeeman <fernando_y767@hotmail.com>
2021-12-03 22:09:17 -0500

    update boilerplate code

--- dayx/dayx-p1.py
+++ dayx/dayx-p1.py
@@ -1,26 +1,31 @@
 import time
 
+
 def main():
-    # inputs = get_input_as_str('input.txt')
-    inputs = get_input_as_str('test.txt')
+    # inputs = standard_func.get_input_as_str('input.txt')
+    inputs = standard_func.get_input_as_str('test.txt')
     
     # Code goes here
 
+
 # Boilerplate code below
+class standard_func:
+    def get_input_as_int(filename):
+        with open(filename) as f:
+            return list(map(lambda a : int(a), list((f.read()).split("\n"))))
 
-def get_input_as_int(filename):
-    with open(filename) as f:
-        return list(map(lambda a : int(a), list((f.read()).split("\n"))))
 
-def get_input_as_str(filename):
-    with open(filename) as f:
-        return list((f.read()).split("\n"))
+    def get_input_as_str(filename):
+        with open(filename) as f:
+            return list((f.read()).split("\n"))
 
-def print_performance(start, end):
-    print('Execution time (s):', round((end - start), 3))
+
+    def print_performance(start, end):
+        print('Execution time (s):', round((end - start), 3))
+
 
 if __name__ == "__main__":
     perf_counter_start = time.perf_counter()
     main()
     perf_counter_end = time.perf_counter()
-    print_performance(perf_counter_start, perf_counter_end)
+    standard_func.print_performance(perf_counter_start, perf_counter_end)
