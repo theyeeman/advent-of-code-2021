ac233a70c9b6bad2b60faddf813420a82cc9b4aa
theyeeman <fernando_y767@hotmail.com>
2021-12-11 21:28:58 -0500

    revert code since accidentally overwrote part 1 solution

--- day10/day10-p1.py
+++ day10/day10-p1.py
@@ -25,7 +25,7 @@ def is_left_bracket(bracket):
     return bracket == '(' or bracket == '[' or bracket == '{' or bracket == '<'
 
 
-def get_missing_brackets(brackets):
+def get_illegal_bracket(brackets):
     stack = []
 
     for bracket in brackets:
@@ -34,49 +34,35 @@ def get_missing_brackets(brackets):
         else:
             latest_stack_bracket = stack.pop()
             if not is_valid_bracket_pair(latest_stack_bracket, bracket):
-                return []
+                return bracket
 
-    return stack[::-1]
-            
+    return None
+
 
 def get_bracket_score(bracket):
     match bracket:
-        case '(':
-            return 1
-        case '[':
-            return 2
-        case '{':
+        case ')':
             return 3
-        case '<':
-            return 4
+        case ']':
+            return 57
+        case '}':
+            return 1197
+        case '>':
+            return 25137
         case _:
             return 0
-
-
-def calculate_score(missing_brackets):
-    score = 0
-
-    for bracket in missing_brackets:
-        score *= 5
-        score += get_bracket_score(bracket)
-
-    return score
 
 
 def main():
     inputs = standard_func.get_input_as_str('input.txt')
     # inputs = standard_func.get_input_as_str('test.txt')
-    score = []
+    score = 0
     
     for input in inputs:
-        missing_brackets = get_missing_brackets(input)
+        illegal_bracket = get_illegal_bracket(input)
+        score += get_bracket_score(illegal_bracket)
 
-        if len(missing_brackets) == 0:
-            continue
-
-        score.append(calculate_score(missing_brackets))
-
-    print(sorted(score)[len(score)//2])
+    print(score)
 
 
 # Boilerplate code below
