806ce0562c71df7f0a6242efd0d404a2211ff040
theyeeman <fernando_y767@hotmail.com>
2021-12-19 20:23:43 -0500

    add brainstorming notes

--- day11/day11-p1.py
+++ day11/day11-p1.py
@@ -27,3 +27,18 @@ if __name__ == "__main__":
     main()
     perf_counter_end = time.perf_counter()
     standard_func.print_performance(perf_counter_start, perf_counter_end)
+
+'''
+Brainstorming
+
+- get input as grid
+    - 2d array of ints
+- value greater than 9 means flash and increase surrounding cells by 1
+- an octopus can only flash at most once per step
+    - flashing means energy back to 0
+- each step all energy increased by 1
+    - so maybe increment all cells and then check for cells > 9, and if yes, set to 0 and increment all surrounding cells
+    - need a way to mark cells that flash so they dont increment again
+- since part 1 asks for 100 steps, part 2 probably asks for an even larger number of steps
+- can't just loop through all cells consecutively since current cell could impact a previous cell
+'''
