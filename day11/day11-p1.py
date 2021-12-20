037d94a1d7d588029e5fb78ebe9bf8da1081a92f
theyeeman <fernando_y767@hotmail.com>
2021-12-19 20:48:27 -0500

    move brainstorm notes to separate file

--- day11/day11-p1.py
+++ day11/day11-p1.py
@@ -104,28 +104,3 @@ if __name__ == "__main__":
     main()
     perf_counter_end = time.perf_counter()
     standard_func.print_performance(perf_counter_start, perf_counter_end)
-
-'''
-Brainstorming
-
-- get input as grid
-    - 2d array of ints
-- value greater than 9 means flash and increase surrounding cells by 1
-- an octopus can only flash at most once per step
-    - flashing means energy back to 0
-- each step all energy increased by 1
-    - so maybe increment all cells and then check for cells > 9, and if yes, set to 0 and increment all surrounding cells
-    - need a way to mark cells that flash so they dont increment again
-- since part 1 asks for 100 steps, part 2 probably asks for an even larger number of steps
-- can't just loop through all cells consecutively since current cell could impact a previous cell
-- have a stack that stores cells > 9, and pop from stack and increment cells around it, then if any of those go greater than 9, push to stack
-    - also need a set to store cell that flashed so it doesn't increment when surrouding cells flashed
-        - actually, if i set cell to 0 after flashing, then i can just check if adjacent cell is 0, if yes, do not increment
-
-List of functins
-- store input as 2d list
-- increment all cells
-- check grid for > 9 cells and store in stack
-- keeping popping from stack and incrementing surrounding cells, and if surrounding cell > 9, add to stack
-- 
-'''
